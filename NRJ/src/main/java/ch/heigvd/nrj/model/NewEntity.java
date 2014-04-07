d7d9561ec21e135bbd19f8c4dacdd19e241fa65f
RJ <erasedlerased@hotmail.com>
2014-04-07 16:43:38 +0200

    History

---
+++ NRJ/src/main/java/ch/heigvd/nrj/model/NewEntity.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.nrj.model;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Robin
+ */
+@Entity
+public class NewEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date timestampMinute;
+
+    public void setTimestampMinute(Date timestampMinute) {
+        this.timestampMinute = timestampMinute;
+    }
+
+    public Date getTimestampMinute() {
+        return timestampMinute;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof NewEntity)) {
+            return false;
+        }
+        NewEntity other = (NewEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ch.heigvd.nrj.model.NewEntity[ id=" + id + " ]";
+    }
+}
