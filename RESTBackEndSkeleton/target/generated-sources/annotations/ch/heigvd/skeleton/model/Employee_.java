6f623d20dd1e01d843406caab31962328f06de0b
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2014-04-07 16:19:13 +0200

    Ajout Plug.java

---
+++ RESTBackEndSkeleton/target/generated-sources/annotations/ch/heigvd/skeleton/model/Employee_.java
@@ -0,0 +1,17 @@
+package ch.heigvd.skeleton.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-04-07T13:27:56")
+@StaticMetamodel(Employee.class)
+public class Employee_ { 
+
+    public static volatile SingularAttribute<Employee, Long> id;
+    public static volatile SingularAttribute<Employee, String> lastName;
+    public static volatile SingularAttribute<Employee, String> email;
+    public static volatile SingularAttribute<Employee, Double> salary;
+    public static volatile SingularAttribute<Employee, String> firstName;
+
+}
