f0743acbf750a4d5c12d3bf2e32e95beafc34d38
Rapou <raphael.schmutz@heig-vd.ch>
2014-04-08 11:48:21 +0200

    Standardisation des TO

--- NRJ/src/main/java/ch/heigvd/nrj/to/PublicApartmentTO.java
+++ NRJ/src/main/java/ch/heigvd/nrj/to/PublicApartmentTO.java
@@ -3,20 +3,11 @@ package ch.heigvd.nrj.to;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * This is an example for a Transfer Object, which will be used to send data to
- * the client. Instances of this class will be created from JPA entities, but
- * will not expose all attributes. In this example, we do not want to send
- * salary information to everyone, so we have stripped this information. In some
- * cases, a transfer object may also be used to aggregate properties from
- * several JPA entities.
- *
- * The class must have an empty constructor, as well as getters and setters for
- * properties (this is required for the JAXB marshalling to work properly). That
- * is also why we have the
+ * This is the transferable object for an Apartment.
  *
  * @XmlRootElement annotation at the class level.
  *
- * @author Olivier Liechti
+ * @author Option40
  */
 @XmlRootElement
 public class PublicApartmentTO {
