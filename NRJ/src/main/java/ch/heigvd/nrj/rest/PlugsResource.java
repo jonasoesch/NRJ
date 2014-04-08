9e121ef29851605b4278384a579aafba8d006029
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2014-04-08 13:52:08 +0200

    Ajout des fichier Resource

---
+++ NRJ/src/main/java/ch/heigvd/nrj/rest/PlugsResource.java
@@ -0,0 +1,15 @@
+package ch.heigvd.nrj.rest;
+
+import ch.heigvd.nrj.model.Plug;
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author nicolas
+ */
+@Stateless
+@Path("plugs")
+public class PlugsResource {
+
+}
