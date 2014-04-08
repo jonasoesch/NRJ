4e16d8f0dd991612bf1e3bdc0ebdc85284538917
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2014-04-08 14:40:46 +0200

    Ajout RoomsResource et RoomsManager

--- NRJ/src/main/java/ch/heigvd/nrj/rest/RoomsResource.java
+++ NRJ/src/main/java/ch/heigvd/nrj/rest/RoomsResource.java
@@ -6,11 +6,17 @@
 
 package ch.heigvd.nrj.rest;
 
+import ch.heigvd.nrj.exceptions.EntityNotFoundException;
 import ch.heigvd.nrj.model.Room;
+import ch.heigvd.nrj.services.crud.RoomsManagerLocal;
+import ch.heigvd.nrj.services.crud.RoomsManager;
+import ch.heigvd.nrj.services.to.RoomsTOServiceLocal;
+import ch.heigvd.nrj.to.PublicRoomTO;
+import java.net.URI;
+import java.util.LinkedList;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -19,6 +25,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -27,5 +36,95 @@ import javax.ws.rs.Produces;
 @Stateless
 @Path("rooms")
 public class RoomsResource {
-   
+    @Context
+    private UriInfo context;
+    @EJB
+    RoomsManagerLocal roomsManager;
+    @EJB
+    RoomsTOServiceLocal roomsTOService;
+
+    /**
+     * Creates a new instance of RoomsResource
+     */
+    public RoomsResource() {
+    }
+
+    /**
+     * Creates a new Room resource from the provided representation
+     *
+     * @return an instance of PublicEmployeeTO
+     */
+    @POST
+    @Consumes({"application/json"})
+    public Response createResource(PublicRoomTO newRoomTO) {
+        Room newRoom = new Room();
+        roomsTOService.updateRoomEntity(newRoom, newRoomTO);
+        long newRoomId = this.roomsManager.create(newRoom);
+        URI createdURI = context.getAbsolutePathBuilder().path(Long.toString(newRoomId)).build();
+        return Response.created(createdURI).build();
+    }
+
+    /**
+     * Retrieves a representation of a list of Room resources
+     *
+     * @return an instance of PublicRoomTO
+     */
+    @GET
+    @Produces({"application/json", "application/xml"})
+    public List<PublicRoomTO> getResourceList() {
+        List<Room> rooms = roomsManager.findAll();
+        List<PublicRoomTO> result = new LinkedList<>();
+        for (Room room : rooms) {
+            result.add(roomsTOService.buildPublicRoomTO(room));
+        }
+        return result;
+    }
+
+    /**
+     * Retrieves representation of an Employee resource
+     *
+     * @param id this id of the employee
+     * @return an instance of PublicEmployeeTO
+     * @throws ch.heigvd.skeleton.exceptions.EntityNotFoundException
+     */
+    @GET
+    @Path("{id}")
+    @Produces({"application/json", "application/xml"})
+    public PublicRoomTO getResource(@PathParam("id") long id) throws EntityNotFoundException {
+        Room room = roomsManager.findById(id);
+        PublicRoomTO roomTO = roomsTOService.buildPublicRoomTO(room);
+        return roomTO;
+    }
+
+    /**
+     * Updates an Employee resource
+     *
+     * @param id this id of the employee
+     * @param updatedEmployeeTO a TO containing the employee data
+     * @return an instance of PublicEmployeeTO
+     * @throws ch.heigvd.skeleton.exceptions.EntityNotFoundException
+     */
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/json"})
+    public Response Resource(PublicRoomTO updatedRoomTO, @PathParam("id") long id) throws EntityNotFoundException {
+        Room roomToUpdate = roomsManager.findById(id);
+        roomsTOService.updateRoomEntity(roomToUpdate, updatedRoomTO);
+        roomsManager.update(roomToUpdate);
+        return Response.ok().build();
+    }
+
+    /**
+     * Deletes an Employee resource
+     *
+     * @param id this id of the employee
+     * @return an instance of PublicEmployeeTO
+     * @throws ch.heigvd.skeleton.exceptions.EntityNotFoundException
+     */
+    @DELETE
+    @Path("{id}")
+    public Response deleteResource(@PathParam("id") long id) throws EntityNotFoundException {
+        roomsManager.delete(id);
+        return Response.ok().build();
+    }
 }
---
+++ NRJ/src/main/java/ch/heigvd/nrj/services/crud/RoomsManager.java
@@ -0,0 +1,57 @@
+package ch.heigvd.nrj.services.crud;
+
+import ch.heigvd.nrj.exceptions.EntityNotFoundException;
+import ch.heigvd.nrj.model.Room;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * DAO service, implementing CRUD operations on the Room entity. 
+ * The class uses the JPA entity manager to interact with the DB. 
+ * It returns JPA entities to its clients.
+ * 
+ * @author nicolas
+ */
+@Stateless
+public class RoomsManager implements RoomsManagerLocal {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public long create(Room roomData) {
+		Room newRoom = new Room(roomData);
+		em.persist(newRoom);
+		return newRoom.getId();
+	}
+
+	@Override
+	public void update(Room newState) throws EntityNotFoundException {
+		em.merge(newState);
+	}
+
+	@Override
+	public void delete(long id) throws EntityNotFoundException {
+		Room roomToDelete = findById(id);
+		em.remove(roomToDelete);
+	}
+
+	@Override
+	public Room findById(long id) throws EntityNotFoundException {
+		Room existingRoom = em.find(Room.class, id);
+		if (existingRoom == null) {
+			throw new EntityNotFoundException();
+		}
+		return existingRoom;
+	}
+
+	@Override
+	public List<Room> findAll() {
+		// Note: the findAllRooms JPQL query is defined in the Room.java file
+		List rooms = em.createNamedQuery("findAllRooms").getResultList();
+		return rooms;
+	}
+	
+}
