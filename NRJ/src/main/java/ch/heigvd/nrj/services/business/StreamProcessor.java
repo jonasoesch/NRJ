fe5e1c7e79b83a5e6a65e495dc89f139616243b0
Nicolas Baudet <nicolas.baudet@heig-vd.ch>
2014-04-10 16:18:40 +0200

    Ajout de la logique pour le StreamProcessor

--- NRJ/src/main/java/ch/heigvd/nrj/services/business/StreamProcessor.java
+++ NRJ/src/main/java/ch/heigvd/nrj/services/business/StreamProcessor.java
@@ -2,6 +2,7 @@ package ch.heigvd.nrj.services.business;
 
 import ch.heigvd.nrj.model.ConsumptionObs;
 import ch.heigvd.nrj.services.crud.ConsumptionsObsManagerLocal;
+import ch.heigvd.nrj.services.crud.RoomConsumptionsFactsManager;
 import javax.ejb.Asynchronous;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -15,6 +16,7 @@ import javax.ejb.Stateless;
 public class StreamProcessor implements StreamProcessorLocal {
     
     @EJB ConsumptionsObsManagerLocal consumptionObsManager;
+    @EJB RoomConsumptionsFactsManager rcfm;
 
     @Override
     public void onConsumption(ConsumptionObs o) {
@@ -23,8 +25,13 @@ public class StreamProcessor implements StreamProcessorLocal {
         // Recording observation
         consumptionObsManager.create(o);
         
-        // Then implement your logic to create facts
+        // Création des facts
+        // Consommation moyenne de cette plug
+        // Récupération des 60 dernières observations et calcul de la moyenne de consommation
+        // Puis ajout à la BD
         
+        // Consommation moyenne de cette pièce
+        // Récupération du dernier fait et ajout de la consommation courante à cette table
     }
     
     /*@Override
