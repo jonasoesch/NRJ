d176eea8fec3cf13c1c2d772442bc23e20e42346
Rapou <raphael.schmutz@heig-vd.ch>
2014-04-10 13:22:22 +0200

    test histories

--- NRJ/src/main/java/ch/heigvd/nrj/rest/TestDataGeneratorResource.java
+++ NRJ/src/main/java/ch/heigvd/nrj/rest/TestDataGeneratorResource.java
@@ -1,11 +1,13 @@
 package ch.heigvd.nrj.rest;
 
 import ch.heigvd.nrj.model.Apartment;
+import ch.heigvd.nrj.model.History;
 import ch.heigvd.nrj.model.Plug;
 import ch.heigvd.nrj.model.PlugConsumptionFact;
 import ch.heigvd.nrj.model.Room;
 import ch.heigvd.nrj.services.crud.ApartmentsManagerLocal;
 import ch.heigvd.nrj.services.crud.EmployeesManagerLocal;
+import ch.heigvd.nrj.services.crud.HistoriesManagerLocal;
 import ch.heigvd.nrj.services.crud.PlugConsumptionsFactsManager;
 import ch.heigvd.nrj.services.crud.PlugConsumptionsFactsManagerLocal;
 import ch.heigvd.nrj.services.crud.PlugsManagerLocal;
@@ -33,6 +35,8 @@ public class TestDataGeneratorResource {
     EmployeesManagerLocal employeesManager;
     @EJB
     PlugsManagerLocal plugsManager;
+    @EJB
+    HistoriesManagerLocal historiesManager;
     @EJB
     RoomsManagerLocal roomsManager;
     @EJB
@@ -71,17 +75,38 @@ public class TestDataGeneratorResource {
         p1.setId(plugsManager.create(p1));
 	m1.addPlug(p1);
 	
+	History h1 = new History();
+        h1.setTimestampMinute(new Date());
+        h1.setStatus(false);
+        h1.setId(historiesManager.create(h1));
+	p1.addHistory(h1);
+	
         Plug p2 = new Plug();
         p2.setName("television");
         p2.setAlwaysOn(false);
         p2.setId(plugsManager.create(p2));
         m2.addPlug(p2);
+	
+	History h2 = new History();
+        h2.setTimestampMinute(new Date());
+        h2.setStatus(false);
+        h2.setId(historiesManager.create(h2));
+	p2.addHistory(h2);
+	
         
         Plug p3 = new Plug();
         p3.setName("lumiere");
         p3.setAlwaysOn(true);
         p3.setId(plugsManager.create(p3));
         m1.addPlug(p3);
+	m1.addPlug(p3);
+	
+	History h3 = new History();
+        h3.setTimestampMinute(new Date());
+        h3.setStatus(true);
+        h3.setId(historiesManager.create(h3));
+	p3.addHistory(h3);
+	
 	
         //Current day time
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
