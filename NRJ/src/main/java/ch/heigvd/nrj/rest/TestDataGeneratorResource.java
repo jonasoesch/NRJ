ac04a0c8e964e940f3b495780a79be6cde466c58
RJ <erasedlerased@hotmail.com>
2014-04-10 09:01:30 +0200

    Serveur de *****

--- NRJ/src/main/java/ch/heigvd/nrj/rest/TestDataGeneratorResource.java
+++ NRJ/src/main/java/ch/heigvd/nrj/rest/TestDataGeneratorResource.java
@@ -1,14 +1,17 @@
 package ch.heigvd.nrj.rest;
 
-
 import ch.heigvd.nrj.model.Apartment;
-import ch.heigvd.nrj.model.Employee;
 import ch.heigvd.nrj.model.Plug;
+import ch.heigvd.nrj.model.PlugConsumptionFact;
 import ch.heigvd.nrj.model.Room;
 import ch.heigvd.nrj.services.crud.ApartmentsManagerLocal;
 import ch.heigvd.nrj.services.crud.EmployeesManagerLocal;
+import ch.heigvd.nrj.services.crud.PlugConsumptionsFactsManager;
 import ch.heigvd.nrj.services.crud.PlugsManagerLocal;
 import ch.heigvd.nrj.services.crud.RoomsManagerLocal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -24,64 +27,65 @@ import javax.ws.rs.Produces;
 @Path("test")
 public class TestDataGeneratorResource {
 
-	final static Logger LOG = Logger.getLogger(TestDataGeneratorResource.class.getName());
+    final static Logger LOG = Logger.getLogger(TestDataGeneratorResource.class.getName());
+    @EJB
+    EmployeesManagerLocal employeesManager;
+    @EJB
+    PlugsManagerLocal plugsManager;
+    @EJB
+    RoomsManagerLocal roomsManager;
+    @EJB
+    ApartmentsManagerLocal apartmentsManager;
+    @EJB
+    PlugConsumptionsFactsManager plugConsumptionsFactsManager;
 
-	@EJB
-	EmployeesManagerLocal employeesManager;
+    @GET
+    @Produces({"text/plain"})
+    public String generateEmployees() throws ParseException {
+        Apartment a = new Apartment();
+        a.setName("Appartement 511");
+        a.setId(apartmentsManager.create(a));
+
+        Room m = new Room();
+        m.setName("Chambre de Barbie");
+        m.setApartment(a);
+        m.setId(roomsManager.create(m));
+
+        Room m2 = new Room();
+        m2.setName("Cuisine");
+        m2.setApartment(a);
+        m2.setId(roomsManager.create(m2));
+
+//		Employee e = new Employee();
+//		e.setFirstName("Elisa");
+//		e.setLastName("Touvomi");
+//		e.setEmail("elisa.touvomi@heig-vd.ch");
+//		e.setSalary(80000);
+//		e.setId(employeesManager.create(e));
+
+        Plug p = new Plug();
+        p.setName("frigo");
+        p.setAlwaysOn(true);
+        p.setRoom(m2);
+        plugsManager.create(p);
+
+        Plug p2 = new Plug();
+        p2.setName("television");
+        p2.setAlwaysOn(false);
+        p2.setRoom(m);
+        plugsManager.create(p2);
         
-        @EJB
-	PlugsManagerLocal plugsManager;
 
-        @EJB
-	RoomsManagerLocal roomsManager;
-        
-        @EJB
-	ApartmentsManagerLocal apartmentsManager;
-        
-	@GET
-  @Produces({"text/plain"})
-	public String generateEmployees() {
-		Apartment a = new Apartment();
-		a.setName("Appartement 511");
-		a.setId(apartmentsManager.create(a));
-		
-		Room m = new Room();
-                m.setName("Chambre de Barbie");
-		m.setApartment(a);
-                m.setId(roomsManager.create(m));
-		
-		Room m2 = new Room();
-                m2.setName("Cuisine");
-		m2.setApartment(a);
-                m2.setId(roomsManager.create(m2));
-		
-		Employee e = new Employee();
-		e.setFirstName("Elisa");
-		e.setLastName("Touvomi");
-		e.setEmail("elisa.touvomi@heig-vd.ch");
-		e.setSalary(80000);
-		e.setId(employeesManager.create(e));
-//                
-//              employeesManager.findAll();
-                
-                Plug p = new Plug();
-                p.setName("frigo");
-                p.setAlwaysOn(true);
-		p.setRoom(m2);
-                plugsManager.create(p);
-                
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        String dateInString = "31-08-1982 10:20:56";
+        Date date = sdf.parse(dateInString);
 
-                Plug p2 = new Plug();
-                p2.setName("television");
-                p2.setAlwaysOn(false);
-                p2.setRoom(m);
-                plugsManager.create(p2);
-                
-//                Apartment a = new Apartment();
-//                p.setName("Chez Mc Cartney");
-//                apartmentsManager.create(a);
-                
+        PlugConsumptionFact pcf1 = new PlugConsumptionFact();
+        pcf1.setPlug(p2);
+        pcf1.setTimestampHour(date);
+        pcf1.setAvgKW(23.00);
+        plugConsumptionsFactsManager.create(pcf1);
 
-		return "done";
-	}
+        return "done";
+    }
 }
