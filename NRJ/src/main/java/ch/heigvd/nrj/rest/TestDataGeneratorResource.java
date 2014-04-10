80276d9b7b9584cfacbc14bf6500900908a141f4
RJ <erasedlerased@hotmail.com>
2014-04-10 13:21:26 +0200

    Warnings on plugZZZZ

--- NRJ/src/main/java/ch/heigvd/nrj/rest/TestDataGeneratorResource.java
+++ NRJ/src/main/java/ch/heigvd/nrj/rest/TestDataGeneratorResource.java
@@ -1,15 +1,18 @@
 package ch.heigvd.nrj.rest;
 
 import ch.heigvd.nrj.model.Apartment;
+import ch.heigvd.nrj.model.History;
 import ch.heigvd.nrj.model.Plug;
 import ch.heigvd.nrj.model.PlugConsumptionFact;
 import ch.heigvd.nrj.model.Room;
+import ch.heigvd.nrj.model.Warning;
 import ch.heigvd.nrj.services.crud.ApartmentsManagerLocal;
 import ch.heigvd.nrj.services.crud.EmployeesManagerLocal;
 import ch.heigvd.nrj.services.crud.PlugConsumptionsFactsManager;
 import ch.heigvd.nrj.services.crud.PlugConsumptionsFactsManagerLocal;
 import ch.heigvd.nrj.services.crud.PlugsManagerLocal;
 import ch.heigvd.nrj.services.crud.RoomsManagerLocal;
+import ch.heigvd.nrj.services.crud.WarningsManagerLocal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -39,11 +42,13 @@ public class TestDataGeneratorResource {
     ApartmentsManagerLocal apartmentsManager;
     @EJB
     PlugConsumptionsFactsManagerLocal plugConsumptionsFactsManager;
+    @EJB
+    WarningsManagerLocal warningManager;
 
     @GET
     @Produces({"text/plain"})
     public String generateEmployees() throws ParseException {
-        
+
         Apartment a = new Apartment();
         a.setName("Appartement 511");
         a.setId(apartmentsManager.create(a));
@@ -51,13 +56,13 @@ public class TestDataGeneratorResource {
         Room m1 = new Room();
         m1.setName("Chambre de Barbie");
         m1.setId(roomsManager.create(m1));
-	a.addRoom(m1);
-	
+        a.addRoom(m1);
+
         Room m2 = new Room();
         m2.setName("Cuisine");
         m2.setId(roomsManager.create(m2));
-	a.addRoom(m2);
-	
+        a.addRoom(m2);
+
 //		Employee e = new Employee();
 //		e.setFirstName("Elisa");
 //		e.setLastName("Touvomi");
@@ -69,30 +74,42 @@ public class TestDataGeneratorResource {
         p1.setName("frigo");
         p1.setAlwaysOn(true);
         p1.setId(plugsManager.create(p1));
-	m1.addPlug(p1);
-	
+        Warning warningp1 = new Warning();
+        warningp1.setMessage("Attention warning du plug1!");
+        warningp1.setTimestampMinute(new Date());
+        warningp1.setId(warningManager.create(warningp1));
+        p1.addWarnings(warningp1);
+        m1.addPlug(p1);
+
         Plug p2 = new Plug();
         p2.setName("television");
         p2.setAlwaysOn(false);
         p2.setId(plugsManager.create(p2));
+        Warning warningp2 = new Warning();
+        warningp2.setMessage("Attention warning du plug2!");
+        warningp2.setTimestampMinute(new Date());
+        warningp2.setId(warningManager.create(warningp2));
         m2.addPlug(p2);
-        
+
         Plug p3 = new Plug();
         p3.setName("lumiere");
         p3.setAlwaysOn(true);
         p3.setId(plugsManager.create(p3));
+        Warning warningp3 = new Warning();
+        warningp3.setMessage("Attention warning du plug3!");
+        warningp3.setTimestampMinute(new Date());
+        warningp3.setId(warningManager.create(warningp3));
         m1.addPlug(p3);
-	
+
         //Current day time
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	Date nowDate = new Date();
-        
+        Date nowDate = new Date();
+
         PlugConsumptionFact pc1 = new PlugConsumptionFact();
         pc1.setTimestampHour(nowDate);
         pc1.setAvgKW(23.00);
         pc1.setPlug(p1);
         pc1.setId(plugConsumptionsFactsManager.create(pc1));
-        System.out.println(pc1.getId());
         p1.addPlugConsumptionFact(pc1);
 
 
