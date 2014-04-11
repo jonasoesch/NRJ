cf3592ac9bc71717e3c66cca462860cd3d5609e7
Chris R <christophe.rast@heig-vd.ch>
2014-04-11 16:07:27 +0200

    www requêtes de base

--- NRJwww/js/nrj.js
+++ NRJwww/js/nrj.js
@@ -8,18 +8,45 @@
 -- ???
 
 */
-$(function() {
+$(function () {
     /* actions au démarrage */
-    
-    
+    getTimegraph();
+
+    getApi('rooms', '');
     /* surveillants */
     /* $('...').on("click", function () {
-    
+
     }) */
-    
-    
+
+
     /* fonctions générales */
     /* function getAllPlugs() {
-    
     } */
+
+    // retrieves the json from the API
+    function getApi(thing, id) {
+        var url = "http://localhost:8080/NRJ/api/" + thing + "/"
+        if (id !== "") {
+            url += id
+        }
+
+        $.get('src/proxy.php', {
+            thing:thing,
+            id:id
+        }, function (json) {
+            console.log(json)
+        });
+    }
+    // retrieves the dom of the timegraph.html
+    function getTimegraph() {
+        var url = "src/timegraph.html"
+        $.get(url, function (html) {
+            var dom = $(html)
+            dom.find('h2').text('TestH2')
+            $('.overall').append(dom)
+        });
+
+    }
+
+
 })
