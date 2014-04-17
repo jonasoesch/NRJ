f0b65278c8e679ae2bdcb4ac264d19e48be7e11e
lfrancelet <lauric.francelet@heig-vd.ch>
2014-04-17 11:20:14 +0200

    Format

--- NRJ/src/main/java/ch/heigvd/nrj/services/to/WarningsTOService.java
+++ NRJ/src/main/java/ch/heigvd/nrj/services/to/WarningsTOService.java
@@ -8,29 +8,30 @@ import javax.ejb.Stateless;
 
 /**
  * This class converts JPA entities into POJO transfer objects, and vice versa
- * 
+ *
  * @author rschmutz
  */
 @Stateless
 public class WarningsTOService implements WarningsTOServiceLocal {
-	@EJB
-	PlugsTOServiceLocal plugsTOService; 
-	
-	@Override
-	public PublicWarningTO buildPublicWarningTO(Warning source) {
-		PublicWarningTO to = new PublicWarningTO(source.getId(), source.getTimestampMinute(), source.getMessage());
-		return to;
-	}
 
-	@Override
-	public void updateWarningEntity(Warning existingEntity, PublicWarningTO newState) {
-		existingEntity.setTimestampMinute(newState.getTimestampMinute());
-		existingEntity.setMessage(newState.getMessage());
-		if(newState.getPlug() != null){
-		    Plug p = new Plug();
-		    p.setId(newState.getPlug().getPlugId());
-		    existingEntity.setPlug(p);
-		}
-	}
-	
+    @EJB
+    PlugsTOServiceLocal plugsTOService;
+
+    @Override
+    public PublicWarningTO buildPublicWarningTO(Warning source) {
+        PublicWarningTO to = new PublicWarningTO(source.getId(), source.getTimestampMinute(), source.getMessage());
+        return to;
+    }
+
+    @Override
+    public void updateWarningEntity(Warning existingEntity, PublicWarningTO newState) {
+        existingEntity.setTimestampMinute(newState.getTimestampMinute());
+        existingEntity.setMessage(newState.getMessage());
+        if (newState.getPlug() != null) {
+            Plug p = new Plug();
+            p.setId(newState.getPlug().getPlugId());
+            existingEntity.setPlug(p);
+        }
+    }
+
 }
