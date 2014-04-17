7f5d38ee549ad645be62d6cd52b18e25330cdc41
Rapou <raphael.schmutz@heig-vd.ch>
2014-04-17 11:37:20 +0200

    Doc TO

--- NRJ/src/main/java/ch/heigvd/nrj/to/PublicApartmentTO.java
+++ NRJ/src/main/java/ch/heigvd/nrj/to/PublicApartmentTO.java
@@ -17,57 +17,117 @@ public class PublicApartmentTO {
     private long apartmentId;
     private String name;
     private List<PublicRoomTO> rooms;
-    private List<PublicApartmentConsumptionFactsTO> apartmentConsumptionFacts; 
-    
+    private List<PublicApartmentConsumptionFactsTO> apartmentConsumptionFacts;
+
+    /**
+     * Build a PublicApartmentTO empty.
+     */
     public PublicApartmentTO() {
 	this.rooms = new ArrayList<>();
 	this.apartmentConsumptionFacts = new ArrayList<>();
     }
 
+    /**
+     * Build a PublicApartmentTO with data.
+     *
+     * @param apartmentId
+     * @param name
+     */
     public PublicApartmentTO(long apartmentId, String name) {
 	this.rooms = new ArrayList<>();
 	this.apartmentConsumptionFacts = new ArrayList<>();
-        this.apartmentId = apartmentId;
-        this.name = name;
+	this.apartmentId = apartmentId;
+	this.name = name;
     }
 
+    /**
+     * Get the id of PublicApartmentTO
+     *
+     * @return the id
+     */
     public long getApartmentId() {
-        return apartmentId;
+	return apartmentId;
     }
 
+    /**
+     * Set the id of PublicApartmentTO
+     *
+     * @param apartmentId
+     */
     public void setApartmentId(long apartmentId) {
-        this.apartmentId = apartmentId;
+	this.apartmentId = apartmentId;
     }
 
+    /**
+     * Get the name of the PublicApartmentTO
+     *
+     * @return the name
+     */
     public String getName() {
-        return name;
+	return name;
     }
 
+    /**
+     * Set the name of the PublicApartmentTO.
+     *
+     * @param name
+     */
     public void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
-    
+
+    /**
+     * Get the rooms of an apartment.
+     *
+     * @return list of rooms
+     */
     public List<PublicRoomTO> getRooms() {
 	return rooms;
     }
 
+    /**
+     * Set the rooms of an apartment.
+     *
+     * @param rooms
+     */
     public void setRooms(List<PublicRoomTO> rooms) {
 	this.rooms = rooms;
     }
-    
+
+    /**
+     * Add a room to the list of apartment's rooms
+     *
+     * @param room
+     */
     public void addRoom(PublicRoomTO room) {
 	this.rooms.add(room);
     }
-    
+
+    /**
+     * Get the list of apartmentConsumptionFacts
+     *
+     * @return list of apartmentConsumptionFacts
+     */
     public List<PublicApartmentConsumptionFactsTO> getApartmentConsumptionFacts() {
 	return apartmentConsumptionFacts;
     }
 
+    /**
+     * Set the apartmentConsumptionFacts
+     *
+     * @param ApartmentConsumptionFacts
+     */
     public void setApartmentConsumptionFacts(List<PublicApartmentConsumptionFactsTO> ApartmentConsumptionFacts) {
 	this.apartmentConsumptionFacts = ApartmentConsumptionFacts;
     }
+
+    /**
+     * Add a apartmentConsumptionFact in the apartment's
+     * apartmentConsumptionFacts
+     *
+     * @param ApartmentConsumptionFact
+     */
     public void addApartmentConsumptionFact(PublicApartmentConsumptionFactsTO ApartmentConsumptionFact) {
 	this.apartmentConsumptionFacts.add(ApartmentConsumptionFact);
     }
-    
 }
