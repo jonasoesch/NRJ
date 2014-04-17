8b4d2e8676d6671e6d00b1eec16a6412da6405ec
Rapou <raphael.schmutz@heig-vd.ch>
2014-04-17 11:16:43 +0200

    Crud IT

--- NRJ/src/main/java/ch/heigvd/nrj/services/crud/ConsumptionsObsManager.java
+++ NRJ/src/main/java/ch/heigvd/nrj/services/crud/ConsumptionsObsManager.java
@@ -16,7 +16,7 @@ import javax.persistence.PersistenceContext;
  * class uses the JPA entity manager to interact with the DB. It returns JPA
  * entities to its clients.
  *
- * @author Olivier Liechti
+ * @author nicolas.
  */
 @Stateless
 public class ConsumptionsObsManager implements ConsumptionsObsManagerLocal {
@@ -26,51 +26,81 @@ public class ConsumptionsObsManager implements ConsumptionsObsManagerLocal {
     @PersistenceContext
     private EntityManager em;
 
+    /**
+     * Create a consumption in persistant DB.
+     *
+     * @param consumption the consumption to create.
+     * @return the id generate.
+     */
     @Override
     public long create(ConsumptionObs consumption) {
-        // Add la consommation à la plug
-        Plug plug = consumption.getPlug();
+	// Add la consommation à la plug
+	Plug plug = consumption.getPlug();
 	System.out.println("hu hu" + plug);
 
 	try {
-            // Rechercher la plug de cette consumption
-            plug = plugsManager.findById(plug.getId());
-        } catch (EntityNotFoundException ex) {
-            System.out.println("ERREUR DANS CONSUMPTIONOBSMANAGER.CREATE(consumption)");
-            Logger.getLogger(ConsumptionsObsManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        // Add la plug à cette consommation
-        consumption.setPlug(plug);
-        em.persist(consumption);
-        plug.addConsumptionObs(consumption);
-        em.flush();
-        return consumption.getId();
+	    // Rechercher la plug de cette consumption
+	    plug = plugsManager.findById(plug.getId());
+	} catch (EntityNotFoundException ex) {
+	    System.out.println("ERREUR DANS CONSUMPTIONOBSMANAGER.CREATE(consumption)");
+	    Logger.getLogger(ConsumptionsObsManager.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	// Add la plug à cette consommation
+	consumption.setPlug(plug);
+	em.persist(consumption);
+	plug.addConsumptionObs(consumption);
+	em.flush();
+	return consumption.getId();
     }
 
+    /**
+     * Update a consumption persistant.
+     *
+     * @param newState the consumption to update.
+     * @throws EntityNotFoundException
+     */
     @Override
     public void update(ConsumptionObs newState) throws EntityNotFoundException {
-        em.merge(newState);
+	em.merge(newState);
     }
 
+    /**
+     * Delete a persistant consumption.
+     *
+     * @param id the id to the persistant consumption to delete
+     * @throws EntityNotFoundException
+     */
     @Override
     public void delete(long id) throws EntityNotFoundException {
-        ConsumptionObs apartmentToDelete = findById(id);
-        em.remove(apartmentToDelete);
+	ConsumptionObs apartmentToDelete = findById(id);
+	em.remove(apartmentToDelete);
     }
 
+    /**
+     * Find a consumption by id.
+     *
+     * @param id the id to the consumption to find.
+     * @return the consumption finded.
+     * @throws EntityNotFoundException
+     */
     @Override
     public ConsumptionObs findById(long id) throws EntityNotFoundException {
-        ConsumptionObs existingConsumption = em.find(ConsumptionObs.class, id);
-        if (existingConsumption == null) {
-            throw new EntityNotFoundException();
-        }
-        return existingConsumption;
+	ConsumptionObs existingConsumption = em.find(ConsumptionObs.class, id);
+	if (existingConsumption == null) {
+	    throw new EntityNotFoundException();
+	}
+	return existingConsumption;
     }
 
+    /**
+     * Find all consumption in persistant DB.
+     *
+     * @return list of all consumptions.
+     */
     @Override
     public List<ConsumptionObs> findAll() {
 // Note: the findAllConsumptions JPQL query is defined in the ConsumptionObs.java file
-        List consumptionsObs = em.createNamedQuery("ConsumptionObs.findAllConsumptionsObs").getResultList();
-        return consumptionsObs;
+	List consumptionsObs = em.createNamedQuery("ConsumptionObs.findAllConsumptionsObs").getResultList();
+	return consumptionsObs;
     }
 }
--- NRJ/src/main/java/ch/heigvd/nrj/services/crud/HistoriesManager.java
+++ NRJ/src/main/java/ch/heigvd/nrj/services/crud/HistoriesManager.java
@@ -23,58 +23,92 @@ public class HistoriesManager implements HistoriesManagerLocal {
 
     @PersistenceContext
     private EntityManager em;
-    
     @EJB
     PlugsManagerLocal plugsManager;
 
+    /**
+     * Create a new History in persistant DB.
+     *
+     * @param historyData the new history.
+     * @return the id generated of the new history.
+     */
     @Override
     public long create(History historyData) {
-        History newHistory = new History(historyData);
+	History newHistory = new History(historyData);
 
-        // Set la plug
-        Plug p = newHistory.getPlug();
-        try {
-            // Rechercher la plug
-            p = plugsManager.findById(p.getId());
-        } catch (EntityNotFoundException ex) {
-            Logger.getLogger(RoomsManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        newHistory.setPlug(p);
-        em.persist(newHistory);
-        p.addHistory(newHistory);
-        em.flush();
-        return newHistory.getId();
+	// Set la plug
+	Plug p = newHistory.getPlug();
+	try {
+	    // Rechercher la plug
+	    p = plugsManager.findById(p.getId());
+	} catch (EntityNotFoundException ex) {
+	    Logger.getLogger(RoomsManager.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	newHistory.setPlug(p);
+	em.persist(newHistory);
+	p.addHistory(newHistory);
+	em.flush();
+	return newHistory.getId();
     }
 
+    /**
+     * Update an history.
+     *
+     * @param newState the history to update.
+     * @throws EntityNotFoundException
+     */
     @Override
     public void update(History newState) throws EntityNotFoundException {
-        em.merge(newState);
+	em.merge(newState);
     }
 
+    /**
+     * Delete an history by it's id.
+     *
+     * @param id the id of the history to delete.
+     * @throws EntityNotFoundException
+     */
     @Override
     public void delete(long id) throws EntityNotFoundException {
-        History apartmentToDelete = findById(id);
-        em.remove(apartmentToDelete);
+	History apartmentToDelete = findById(id);
+	em.remove(apartmentToDelete);
     }
 
+    /**
+     * Find an history by his ID.
+     *
+     * @param id the history id
+     * @return an history find.
+     * @throws EntityNotFoundException
+     */
     @Override
     public History findById(long id) throws EntityNotFoundException {
-        History existingHistorie = em.find(History.class, id);
-        if (existingHistorie == null) {
-            throw new EntityNotFoundException();
-        }
-        return existingHistorie;
+	History existingHistorie = em.find(History.class, id);
+	if (existingHistorie == null) {
+	    throw new EntityNotFoundException();
+	}
+	return existingHistorie;
     }
 
+    /**
+     * Find all histories in persistant DB
+     *
+     * @return list of all persistant histories.
+     */
     @Override
     public List<History> findAll() {
 // Note: the findAllHistories JPQL query is defined in the Historie.java file
-        List apartments = em.createNamedQuery("Histories.findAllHistories").getResultList();
-        return apartments;
+	List apartments = em.createNamedQuery("Histories.findAllHistories").getResultList();
+	return apartments;
     }
 
+    /**
+     * Find last History in persistant DB.
+     *
+     * @return the last history.
+     */
     public History findLast() {
-        List<History> last = em.createNamedQuery("Histories.findLast").getResultList();
-        return last.get(0);
+	List<History> last = em.createNamedQuery("Histories.findLast").getResultList();
+	return last.get(0);
     }
 }
